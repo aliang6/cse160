7d25999e4da481f8f52158db803cf5147ced0e12
Andy Liang <andy.liang.1@stonybrook.edu>
2017-02-08 21:41:03 -0500

    Homework 3

---
+++ Homework3/mail-system/MailItem.java
@@ -0,0 +1,73 @@
+/**
+ * A class to model a simple mail item. The item has sender and recipient
+ * addresses and a message string.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2011.07.31
+ */
+public class MailItem
+{
+    // The sender of the item.
+    private String from;
+    // The intended recipient.
+    private String to;
+    // The text of the message.
+    private String message;
+    // The email of the sender
+    private String email;
+
+    /**
+     * Create a mail item from sender to the given recipient,
+     * containing the given message.
+     * @param from The sender of this item.
+     * @param to The intended recipient of this item.
+     * @param message The text of the message to be sent.
+     */
+    public MailItem(String from, String to, String message, String email)
+    {
+        this.from = from;
+        this.to = to;
+        this.message = message;
+        this.email = email;
+    }
+
+    /**
+     * @return The sender of this message.
+     */
+    public String getFrom()
+    {
+        return from;
+    }
+
+    /**
+     * @return The intended recipient of this message.
+     */
+    public String getTo()
+    {
+        return to;
+    }
+
+    /**
+     * @return The text of the message.
+     */
+    public String getMessage()
+    {
+        return message;
+    }
+    
+    public String getEmail()
+    {
+        return email;
+    }
+
+    /**
+     * Print this mail message to the text terminal.
+     */
+    public void print()
+    {
+        System.out.println("Subject: " + email);
+        System.out.println("From: " + from);
+        System.out.println("To: " + to);
+        System.out.println("Message: " + message);
+    }
+}
